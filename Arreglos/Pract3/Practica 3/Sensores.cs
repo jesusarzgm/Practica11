2ee28bb0374a273392e25c93589ce4a271bc4735
jesusarzgm <42191570+jesusarzgm@users.noreply.github.com>
2018-12-06 18:48:30 -0600

    Add files via upload

---
+++ Arreglos/Pract3/Practica 3/Sensores.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolimorfismoPract3
+{
+    abstract class Sensores
+    {
+        protected int Distancia;
+
+        public abstract string Avanzar();
+
+        public abstract string Detenerse();
+
+        public virtual string Alarma()
+        {
+            return "Alarma Activada";
+        }
+    }
+}
