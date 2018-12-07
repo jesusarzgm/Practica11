862dcf1c1a6fc8bb2eeb5da58b10ec7733649194
jesusarzgm <42191570+jesusarzgm@users.noreply.github.com>
2018-12-06 19:19:21 -0600

    Add files via upload

---
+++ PolimorfismoPract3/PolimorfismoPract3/SonicoD.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolimorfismoPract3
+{
+      class SonicoD:Sensores
+    {
+        public SonicoD()
+        {
+            this.Distancia = 0;
+        }
+        public SonicoD(int d)
+        {
+            this.Distancia = d;
+        }
+
+        public override string Avanzar()
+        {
+            if (100 < this.Distancia)
+            {
+                return "Estoy avanzando hacia adelante\n";
+            }
+            else
+            {
+                return Detenerse();
+            }
+           
+        }
+        public override string Detenerse()
+        {
+            return "Me detuve \n";
+        }
+    }
+}
---
+++ PolimorfismoPract3/PolimorfismoPract3/SonicoR.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolimorfismoPract3
+{
+     class SonicoR:Sensores
+    {
+        public SonicoR()
+        {
+            this.Distancia = 0;
+        }
+        public SonicoR(int d)
+        {
+            this.Distancia = d;
+        }
+
+        public override string Avanzar()
+        {
+            if (this.Distancia > 50)
+            {
+
+                return "Estoy avanzando de reversa\n";
+            }
+            else
+            {
+                return Detenerse();
+
+            }
+
+
+        }
+        public override string Detenerse()
+        {
+            return "Me detuve \n"+ this.Alarma()+"\n";
+        }
+    }
+}
